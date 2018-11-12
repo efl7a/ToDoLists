e02110cabe1d9f2e490d864c38616f0f34067a7e
Heather Christman <heatherchristman@cpe-2606-a000-c690-8700-1407-90a8-b9c-d731.dyn6.twc.com>
2018-11-12 14:37:01 -0500

    tableview check/uncheck functionality complete

---
+++ ToDoLists/ToDoListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  ToDoLists
+//
+//  Created by Heather Christman on 11/12/18.
+//  Copyright © 2018 Heather Christman. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["make lunch", "reset iphone", "finish course"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
